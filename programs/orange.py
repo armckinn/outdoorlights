af98594d14695e99b6d4fc4dd421a3153c64436c
Sandy <Daddy@Aquaman.mckinneys.ca>
2018-10-16 23:57:21 -0400

    Implement linear down fading.

--- programs/orange.py
+++ programs/orange.py
@@ -7,6 +7,7 @@ from array import array
 from ola.ClientWrapper import ClientWrapper
 from ola.DMXConstants import DMX_MIN_SLOT_VALUE, DMX_MAX_SLOT_VALUE,  DMX_UNIVERSE_SIZE
 import math
+import time
 
 __author__ = 'Alexander McKinney'
 
@@ -16,7 +17,7 @@ Fade up to Orange on tree lights.
 
 
 UPDATE_INTERVAL = 25  # In ms, this comes about to ~40 frames a second
-FINISH_TIME = 10000    # In ms, when the fade should be done.
+FINISH_TIME = 1000    # In ms, when the fade should be done.
 DMX_DATA_SIZE = 100
 UNIVERSE = 1
 R_TREE_LIGHTS_CH = [1, 5, 9, 13, 17, 101, 105, 109, 113, 117]
@@ -29,14 +30,21 @@ class LinearFade(object):
         self._end_value = end_value
         self._num_steps = num_steps
         # Calculate linearizing factor: https://diarmuid.ie/blog/pwm-exponential-led-fading-on-arduino-or-other-platforms/
-        self._R = (num_steps * math.log10(2))/(math.log10(end_value-start_value));
+        self._R = (num_steps * math.log10(2))/(math.log10(abs(end_value-start_value)));
+        if end_value > start_value:
+            self._up_dir = True
+        else:
+            self._up_dir = False
 
     def UpdateLights(self, data, cur_step):
         """
         This function gets called periodically based on UPDATE_INTERVAL
         """
         for light in self._lights:
-            data[light-1] = int(self._start_value + 2**(cur_step / self._R) - 1);
+            if self._up_dir:
+                data[light-1] = int(self._start_value + 2**(cur_step / self._R) - 1);
+            else:
+                data[light-1] = int(self._end_value + 2**((self._num_steps - cur_step) / self._R) - 1);
             #data[light-1] = int((self._end_value - self._start_value)*1.0/steps * cur_step)
 
 
@@ -74,16 +82,21 @@ class DMXUpdater(object):
 
 if __name__ == '__main__':
     # Setup to Faders.
-    r_fader = LinearFade(R_TREE_LIGHTS_CH, 10, 255, FINISH_TIME/UPDATE_INTERVAL)
-    g_fader = LinearFade(G_TREE_LIGHTS_CH, 5, 165, FINISH_TIME/UPDATE_INTERVAL)
-
-    progs = [ r_fader, g_fader ]
+    r_fader_up = LinearFade(R_TREE_LIGHTS_CH, 10, 255, FINISH_TIME/UPDATE_INTERVAL)
+    g_fader_up = LinearFade(G_TREE_LIGHTS_CH, 5, 165, FINISH_TIME/UPDATE_INTERVAL)
+    progs_up = [ r_fader_up, g_fader_up ]
+    r_fader_down = LinearFade(R_TREE_LIGHTS_CH, 255, 10, FINISH_TIME/UPDATE_INTERVAL)
+    g_fader_down = LinearFade(G_TREE_LIGHTS_CH, 165, 5, FINISH_TIME/UPDATE_INTERVAL)
+    progs_down = [ r_fader_down, g_fader_down ]
 
     wrapper = ClientWrapper()
-    controller = DMXUpdater(UNIVERSE, UPDATE_INTERVAL, wrapper, progs, FINISH_TIME/UPDATE_INTERVAL)
-    # Set things to stop after the number of steps.
-    #g_wrapper.AddEvent(SHUTDOWN_INTERVAL, g_wrapper.Stop)
-    #r_wrapper.AddEvent(SHUTDOWN_INTERVAL, r_wrapper.Stop)
-    wrapper.Run()
+    while True:
+        controller = DMXUpdater(UNIVERSE, UPDATE_INTERVAL, wrapper, progs_up, FINISH_TIME/UPDATE_INTERVAL)
+        wrapper.Run()
+
+        time.sleep(2)
+
+        controller = DMXUpdater(UNIVERSE, UPDATE_INTERVAL, wrapper, progs_down, FINISH_TIME/UPDATE_INTERVAL)
+        wrapper.Run()
 
     print "Done"
