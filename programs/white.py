28a1995a6f1457b3b8d816cd5e9c9079ccd32d6d
Alexander McKinney <armckinn@mckinneys.ca>
2018-10-29 16:01:14 -0400

    Added off (i.e. black.py). Toned down the orange brightness. Did a more linear white.

--- programs/white.py
+++ programs/white.py
@@ -6,6 +6,8 @@
 from array import array
 from ola.ClientWrapper import ClientWrapper
 from ola.DMXConstants import DMX_MIN_SLOT_VALUE, DMX_MAX_SLOT_VALUE,  DMX_UNIVERSE_SIZE
+import math
+import datetime
 
 __author__ = 'Alexander McKinney'
 
@@ -20,29 +22,62 @@ DMX_DATA_SIZE = 100
 UNIVERSE = 1
 TREE_LIGHTS_CH = [4, 8, 12, 16, 20, 104, 108, 112, 116, 120]
 
-class SimpleFadeController(object):
+class LinearFade(object):
+    def __init__(self, lights, start_value, end_value, num_steps):
+        self._lights = lights
+        self._start_value = start_value
+        self._end_value = end_value
+        self._num_steps = num_steps
+        # Calculate linearizing factor: https://diarmuid.ie/blog/pwm-exponential-led-fading-on-arduino-or-other-platforms/
+        self._R = (num_steps * math.log10(2))/(math.log10(abs(end_value-start_value)));
+        if end_value > start_value:
+            self._up_dir = True
+        else:
+            self._up_dir = False
+
+    def UpdateLights(self, data, cur_step):
+        """
+        This function gets called periodically based on UPDATE_INTERVAL
+        """
+        for light in self._lights:
+            if self._up_dir:
+                data[light-1] = int(self._start_value + 2**(cur_step / self._R) - 1);
+            else:
+                data[light-1] = int(self._end_value + 2**((self._num_steps - cur_step) / self._R) - 1);
+            #data[light-1] = int((self._end_value - self._start_value)*1.0/steps * cur_step)
+
+class Hold(object):
+    def __init__(self, lights, value):
+        self._lights = lights
+        self._value = value
+
+    def UpdateLights(self, data, cur_step):
+        """
+        This function gets called periodically based on UPDATE_INTERVAL
+        """
+        for light in self._lights:
+            data[light-1] = self._value
+
+
+class DMXUpdater(object):
   def __init__(self, universe, update_interval, client_wrapper,
-               lights, start_value, end_value, steps):
+               progs, steps):
     self._universe = universe
     self._update_interval = update_interval
-    self._data = array('B', [DMX_MIN_SLOT_VALUE] * max(lights))
-    self._lights = lights
-    self._start_value = start_value
-    self._end_value = end_value
+    self._data = array('B', [DMX_MIN_SLOT_VALUE] * 150)
+    self._progs = progs
     self._steps = steps
     self._cur_step = 0
     self._wrapper = client_wrapper
     self._client = client_wrapper.Client()
     self._wrapper.AddEvent(self._update_interval, self.UpdateDmx)
-    print max(lights)
-    print len(self._data)
 
   def UpdateDmx(self):
     """
     This function gets called periodically based on UPDATE_INTERVAL
     """
-    for light in self._lights:
-        self._data[light-1] = int((self._end_value - self._start_value)*1.0/self._steps * self._cur_step)
+    for prog in self._progs:
+        prog.UpdateLights(self._data, self._cur_step)
     self._cur_step += 1;
 
     if self._cur_step <= self._steps:
@@ -56,11 +91,23 @@ class SimpleFadeController(object):
 
 
 if __name__ == '__main__':
+    fader_up = LinearFade(TREE_LIGHTS_CH, 10, 180, FINISH_TIME/UPDATE_INTERVAL)
+    progs_up = [ fader_up ]
+    hold = Hold(TREE_LIGHTS_CH, 180)
+    fader_down = LinearFade(TREE_LIGHTS_CH, 180, 0, FINISH_TIME/UPDATE_INTERVAL)
+    progs_down = [ fader_down ]
+
+    print "%s: Ramp up" % (str(datetime.datetime.now()))
     wrapper = ClientWrapper()
-    controller = SimpleFadeController(UNIVERSE, UPDATE_INTERVAL, wrapper, TREE_LIGHTS_CH, 0, 255,
-                                        FINISH_TIME/UPDATE_INTERVAL)
-    # Set things to stop after the number of steps.
-    #wrapper.AddEvent(SHUTDOWN_INTERVAL, r_wrapper.Stop)
+    controller = DMXUpdater(UNIVERSE, UPDATE_INTERVAL, wrapper, progs_up, FINISH_TIME/UPDATE_INTERVAL)
+    wrapper.Run()
+
+    print "%s: Hold" % (str(datetime.datetime.now()))
+    controller = DMXUpdater(UNIVERSE, UPDATE_INTERVAL, wrapper, [ hold ], 7*60*60*1000/UPDATE_INTERVAL)
+    wrapper.Run()
+
+    print "%s: Ramp down" % (str(datetime.datetime.now()))
+    controller = DMXUpdater(UNIVERSE, UPDATE_INTERVAL, wrapper, progs_down, FINISH_TIME/UPDATE_INTERVAL)
     wrapper.Run()
 
     print "Done"
